124630c9ad890bc385ba4727f821774f4cbc0fbe
Jonathan Cowling <09cowlingj@gmail.com>
2019-06-09 05:06:58 +0100

    Web can communicate over bluetooth

--- app/src/static/js/bluetooth.js
+++ app/src/static/js/bluetooth.js
@@ -1,9 +1,20 @@
-var bluetoothName = "bluetooth_buddy"
+function str2ab(str) {
+  var buf = new ArrayBuffer(str.length * 2);
+  var bufView = new Uint16Array(buf);
+  for (var i = 0, strLen = str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
+
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
 
 function writeKeyToDevice(key) {
   return navigator.bluetooth.requestDevice({
     acceptAllDevices: true,
-    //filters: [{ name: bluetoothName }]
+    optionalServices: [0x1825]
   }).then(function (device) {
     return device.gatt.connect();
   }).then(function (server) {
@@ -11,7 +22,7 @@ function writeKeyToDevice(key) {
   }).then(function (service) {
     return service.getCharacteristic(0x2A3D);
   }).then(function (characteristic) {
-    return characteristic.writeValue(new Buffer(key));
+    return characteristic.writeValue(str2ab(key));
   }).catch(function (error) {
     console.error('Connection failed!', error);
   });
@@ -21,7 +32,7 @@ function readKeyFromDevice() {
 
   return navigator.bluetooth.requestDevice({
     acceptAllDevices: true,
-    //filters: [{ name: bluetoothName }]
+    optionalServices: [0x1825],
   }).then(function (device) {
     return device.gatt.connect();
   }).then(function (server) {
@@ -30,6 +41,8 @@ function readKeyFromDevice() {
     return service.getCharacteristic(0x2A3D);
   }).then(function (characteristic) {
     return characteristic.readValue();
+  }).then(function (dataView) {
+     return ab2str(dataView.buffer)
   }).catch(function (error) {
     console.error('Connection failed!', error);
   });
